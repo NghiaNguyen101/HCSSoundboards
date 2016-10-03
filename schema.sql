82f3e61f368e4e56bfc6e0c1c762e06ede32de75
Mattea Cavagnaro <mfcavagnaro@gmail.com>
2016-10-03 12:44:33 -0700

    updated sql file

--- schema.sql
+++ schema.sql
@@ -1,9 +1,19 @@
+CREATE database soundboard;
+use soundboard;
+
 CREATE TABLE sound
 (
   id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   sound MEDIUMBLOB NOT NULL,
   size INT(11) NOT NULL
+);
+CREATE TABLE user
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  username VARCHAR(45) NOT NULL,
+  password VARCHAR(60) NOT NULL,
+  enabled TINYINT(4) DEFAULT '1' NOT NULL
 );
 CREATE TABLE role
 (
@@ -14,10 +24,3 @@ CREATE TABLE role
 );
 CREATE INDEX fk_username ON role (userId);
 CREATE UNIQUE INDEX uni_username_role ON role (role, userId);
-CREATE TABLE user
-(
-  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  username VARCHAR(45) NOT NULL,
-  password VARCHAR(60) NOT NULL,
-  enabled TINYINT(4) DEFAULT '1' NOT NULL
-);
