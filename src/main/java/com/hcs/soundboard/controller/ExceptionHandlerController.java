dc323850c4b63575b93ca8d3756622dd0c5e53bf
NghiaNguyen101 <nnguy101@gmail.com>
2016-10-14 14:49:05 -0700

    Add 404 page

---
+++ src/main/java/com/hcs/soundboard/controller/ExceptionHandlerController.java
@@ -0,0 +1,33 @@
+package com.hcs.soundboard.controller;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Handler error page
+ * Replace the whitelabel error page from spring with approriate error page
+ *
+ */
+
+@Controller
+public class ExceptionHandlerController implements ErrorController{
+
+    private static final String PATH = "/error";
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @RequestMapping(value = PATH)
+    public String error() {
+
+        return "404";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
---
+++ src/main/webapp/404.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ include file="/includes.jsp" %>
+
+<hcs:standard-page title="HCS Soundboards" page="home">
+    <div class="jumbotron">
+        <h1>HCS Soundboards</h1>
+        <p class="lead">View soundboards and create your own.</p>
+        <br>
+        <br>
+        <h2>404 Page</h2>
+        <br>
+        <br>
+        <h2>Search??</h2>
+    </div>
+
+
+</hcs:standard-page>
